ad9bebf5b33ba7f97349c8a40de62c409e2eb7b5
rma6 <rma6@cin.ufpe.br>
2019-10-07 17:52:41 -0300

    q4 in progress

--- lista1/q4.hs
+++ lista1/q4.hs
@@ -4,37 +4,46 @@ data Tree a = Nilt |
 
 type Set a = Tree a
 
---instance Show Set a where
---    Show (Set a) = 
+--instance Eq a => Eq (Set a) where
+--    (==) = eqSet
+
+--instance Ord a => Ord (Set a) where
+--    (<=) = leqSet
+
+empty :: Set a
+empty = Nilt
+
+sing :: a -> Set a
+sing a = Node a Nilt Nilt
 
 --checa se um valor pentence ao conjunto
-has :: (Eq a, Ord a) => Set a -> a -> Bool
-has Nilt _ = False
-has (Node a tl tr) v | v==a = True
-                     | v<a = has tl v
-                     | v>a = has tr v
+memSet :: Ord a => Set a -> a -> Bool
+memSet Nilt _ = False
+memSet (Node a tl tr) v | v==a = True
+                     | v<a = memSet tl v
+                     | v>a = memSet tr v
 
 --insere um elemento no conjunto
-insert :: (Eq a, Ord a) => Set a -> a -> Set a
+insert :: Ord a => Set a -> a -> Set a
 insert Nilt v = (Node v Nilt Nilt)
-insert (Node a tl tr) v | has (Node a tl tr) v = (Node a tl tr)
+insert (Node a tl tr) v | memSet (Node a tl tr) v = (Node a tl tr)
                         | v<a = Node a (insert tl v) tr
                         | v>a = Node a tl (insert tr v)
 
 --função auxiliar de remove. No caso do nó a ser removido ter 2 filho, utiliza-se essa função para retornar a árvore à direita do nó a ser removido com o valor mínimo dela agora na raiz
-minRightTree :: (Eq a, Ord a) => Set a -> Set a
+minRightTree :: Ord a => Set a -> Set a
 minRightTree (Node a tl tr) = Node (findMinRightTree tr) tl (remove tr (findMinRightTree tr))
 
 --Retorna o menor elemento da árvore da direita
-findMinRightTree :: (Eq a, Ord a) => Set a -> a
+findMinRightTree :: Ord a => Set a -> a
 findMinRightTree (Node a Nilt Nilt) = a
 findMinRightTree (Node a Nilt tr) = a
 findMinRightTree (Node a tl tr) = findMinRightTree tl
 
 --remove um elemento do conjunto
-remove :: (Eq a, Ord a) => Set a -> a -> Set a
+remove :: Ord a => Set a -> a -> Set a
 remove (Node a Nilt Nilt) _ = Nilt
-remove (Node a tl tr) v | not (has (Node a tl tr) v) = (Node a tl tr)
+remove (Node a tl tr) v | not (memSet (Node a tl tr) v) = (Node a tl tr)
                         | v<a = Node a (remove tl v) tr
                         | v>a = Node a tl (remove tr v)
                         | v==a && tl/=Nilt && tr/=Nilt = (minRightTree (Node a tl tr))
@@ -42,4 +51,14 @@ remove (Node a tl tr) v | not (has (Node a tl tr) v) = (Node a tl tr)
                         | v==a && tl==Nilt && tr/=Nilt = tr
                         | v==a && tl==Nilt && tr==Nilt = Nilt
 
---Set2List :: (Ord a) => Set a -> [a]
+
+makeSet :: Ord a => [a] -> Set a
+makeSet [] = Nilt
+makeSet (a:ax)
+
+set2List :: Ord a => Set a -> [a]
+set2List (Nilt) = []
+set2List (Node a tl tr) = (set2List tl)++[a]++(set2List tr)
+
+card :: Ord a => Set a -> Int
+card s = length (set2List s)
