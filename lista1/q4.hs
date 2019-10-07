5c7f45664c9daf0a1b17e6e5e621e6c5b1cd11cc
rma6 <rma6@cin.ufpe.br>
2019-10-07 20:42:49 -0300

    q4 finished

--- lista1/q4.hs
+++ lista1/q4.hs
@@ -4,12 +4,6 @@ data Tree a = Nilt |
 
 type Set a = Tree a
 
---instance Eq a => Eq (Set a) where
---    (==) = eqSet
-
---instance Ord a => Ord (Set a) where
---    (<=) = leqSet
-
 empty :: Set a
 empty = Nilt
 
@@ -51,14 +45,49 @@ remove (Node a tl tr) v | not (memSet (Node a tl tr) v) = (Node a tl tr)
                         | v==a && tl==Nilt && tr/=Nilt = tr
                         | v==a && tl==Nilt && tr==Nilt = Nilt
 
-
+--cria um conjunto a partir de uma lista
 makeSet :: Ord a => [a] -> Set a
-makeSet [] = Nilt
-makeSet (a:ax)
+makeSet l = makeSetSeed empty l
 
+--função auxiliar para makeSet
+makeSetSeed :: Ord a => Set a -> [a] -> Set a
+makeSetSeed s [] = s
+makeSetSeed s (a:ax) = makeSetSeed (insert s a) ax
+
+--converte um conjunto para uma lista
 set2List :: Ord a => Set a -> [a]
 set2List (Nilt) = []
 set2List (Node a tl tr) = (set2List tl)++[a]++(set2List tr)
 
+--retorna a cardinalidade de um conjunto
 card :: Ord a => Set a -> Int
 card s = length (set2List s)
+
+--verifica se 2 conjuntos são iguais
+eqSet :: Ord a => Set a -> Set a -> Bool
+eqSet a b = (set2List a) == (set2List b)
+
+--realiza operação união entre 2 conjuntos
+union :: Ord a => Set a -> Set a -> Set a
+union a b = makeSet ((set2List a)++(set2List b))
+
+--realiza operação interseção entre 2 conjuntos
+inter :: Ord a => Set a -> Set a -> Set a
+inter a b = makeSet (filter (\x -> memSet b x) (set2List a))
+
+--reliza opereção diferença entre 2 conjuntos
+diff :: Ord a => Set a -> Set a -> Set a
+diff a b = makeSet (filter (\x -> not(memSet b x)) (set2List a))
+
+--checa se um conjunto é subconjunto de outro conjunto
+subSet :: Ord a => Set a -> Set a -> Bool
+subSet a b = eqSet (union a b) b
+
+mapSet :: (Ord a, Ord b) => (a -> b) -> Set a -> Set b
+mapSet f s = makeSet (map f (set2List s))
+
+filterSet :: Ord a => (a -> Bool) -> Set a -> Set a
+filterSet p s = makeSet (filter p (set2List s))
+
+foldSet :: Ord a => (a -> a -> a) -> a -> Set a -> a
+foldSet f x s = (foldr f x (set2List s))
