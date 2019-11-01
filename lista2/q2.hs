8fb481d9f8cf5d9ee09d816a69f60e7f7166e470
rma6 <rma6@cin.ufpe.br>
2019-11-01 15:55:06 -0300

    lista2/q2 in progress

---
+++ lista2/q2.hs
@@ -0,0 +1,12 @@
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+--A ⊆ B if and only if A ∩ B = A
+
+--produto cartesiano A × (B ∪ C) = (A × B) ∪ (A × C)
+
+--if A ⊆ B then A \ B = ∅
+
+--diferença simétrica AΔB = (A - B) ∪ (B - A)
+
+-- | A × B | = | B × A | = | A | × | B |
